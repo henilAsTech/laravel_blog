e6a9eac5a57c0d715da79f1c7e5d63e417d4e0d8
henilAsTech <henil.astech@gmail.com>
2022-06-06 13:18:28 +0530

    database

--- laravel_blog.sql
+++ laravel_blog.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.4
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Jun 02, 2022 at 02:59 PM
--- Server version: 10.4.17-MariaDB
--- PHP Version: 7.3.27
+-- Host: localhost
+-- Generation Time: Jun 06, 2022 at 09:47 AM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,17 +24,40 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `admins`
+--
+
+CREATE TABLE `admins` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `role_as` int(11) NOT NULL,
+  `username` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `picture` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `admins`
+--
+
+INSERT INTO `admins` (`id`, `role_as`, `username`, `email`, `password`, `picture`, `created_at`, `updated_at`) VALUES
+(1, 1, 'admin', 'admin@gmail.com', '1234', '', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `blogs`
 --
 
 CREATE TABLE `blogs` (
   `id` bigint(20) UNSIGNED NOT NULL,
-  `user_id` bigint(20) UNSIGNED DEFAULT NULL,
+  `user_id` bigint(20) UNSIGNED NOT NULL,
   `title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `description` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `picture` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `status` bigint(20) UNSIGNED NOT NULL DEFAULT 1,
+  `picture` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -43,10 +66,29 @@ CREATE TABLE `blogs` (
 -- Dumping data for table `blogs`
 --
 
-INSERT INTO `blogs` (`id`, `user_id`, `title`, `type`, `description`, `picture`, `status`, `created_at`, `updated_at`) VALUES
-(1, NULL, 'arfg', 'reswg', 'ewg', 'C:\\xampp-latest\\tmp\\php1E61.tmp', 1, '2022-06-02 05:11:06', '2022-06-02 05:11:06'),
-(2, NULL, 'ewf', 'earg', 'adsg', 'C:\\xampp-latest\\tmp\\phpC197.tmp', 1, '2022-06-02 05:11:48', '2022-06-02 05:11:48'),
-(3, 2, 'nikul', 'nikuls blog', 'this is personal blog', 'C:\\xampp-latest\\tmp\\php3E9D.tmp', 1, '2022-06-02 05:33:05', '2022-06-02 05:33:05');
+INSERT INTO `blogs` (`id`, `user_id`, `title`, `type`, `description`, `picture`, `created_at`, `updated_at`) VALUES
+(1, 1, 'Laravel', 'Education', 'Laravel is a free, open-source[3] PHP web framework, created by Taylor Otwell and intended for the development of web applications following the model–view–controller (MVC) architectural pattern and based on Symfony. Some of the features of Laravel are a modular packaging system with a dedicated dependency manager, different ways for accessing relational databases, utilities that aid in application deployment and maintenance, and its orientation toward syntactic sugar.[', 'image/blog/1734865297443952.png', '2022-06-05 22:38:40', '2022-06-06 00:51:16'),
+(2, 1, 'Java', 'Education', 'Java is a high-level, class-based, object-oriented programming language that is designed to have as few implementation dependencies as possible. It is a general-purpose programming language intended to let programmers write once, run anywhere (WORA),[17] meaning that compiled Java code can run on all platforms that support Java without the need to recompile.[18] Java applications are typically compiled to bytecode that can run on any Java virtual machine (JVM) regardless of the underlying computer architecture. The syntax of Java is similar to C and C++, but has fewer low-level facilities than either of them. The Java runtime provides dynamic capabilities (such as reflection and runtime code modification) that are typically not available in traditional compiled languages. As of 2019, Java was one of the most popular programming languages in use according to GitHub,[19][20] particularly for client–server web applications, with a reported 9 million developers', 'image/blog/1734857020080929.png', '2022-06-05 22:39:42', '2022-06-05 22:39:42'),
+(3, 2, 'Cricket', 'Sports', 'Cricket is a bat-and-ball game played between two teams of eleven players each on a field at the centre of which is a 22-yard (20-metre) pitch with a wicket at each end, each comprising two bails balanced on three stumps. The game proceeds when a player on the fielding team, called the bowler, \"bowls\" (propels) the ball from one end of the pitch towards the wicket at the other end, with an \"over\" being completed once they have legally done so six times. The batting side has one player at each end of the pitch, with the player at the opposite end of the pitch from the bowler aiming to strike the ball with a bat. The batting side scores runs when either the bowler unfairly bowls the ball to the batter, the ball reaches the boundary of the field', 'image/blog/1734857108412932.jpg', '2022-06-05 22:41:06', '2022-06-05 22:41:06'),
+(4, 2, 'Caram', 'Sports', 'Caram was drafted by Meralco Bolts of the Philippine Basketball Association (PBA). In the 2013-14 Philippine Cup, he finished with seven points, three rebounds and three assists, plus a late three-pointer in the fourth that iced the win for Meralco against the Alaska Aces.[3]\r\n\r\nIn the 2017-18 Philippine Cup, Caram had 16 points in their first game against Barangay Ginebra since their Finals series, where Meralco lost.[4]\r\n\r\nIn the 2019 Governors\' Cup, he became the shortest player to score 30 points in a game, with 16 of those points coming in the fourth quarter.[5] For that performance, he was named Player of the Week.[6] Following that conference, he was signed to a two-year extension.', 'image/blog/1734857198099560.jpg', '2022-06-05 22:42:32', '2022-06-05 22:42:32'),
+(5, 3, 'PHP', 'Education', 'PHP code is usually processed on a web server by a PHP interpreter implemented as a module, a daemon or as a Common Gateway Interface (CGI) executable. On a web server, the result of the interpreted and executed PHP code – which may be any type of data, such as generated HTML or binary image data – would form the whole or part of an HTTP response. Various web template systems, web content management systems, and web frameworks exist which can be employed to orchestrate or facilitate the generation of that response. Additionally, PHP can be used for many programming tasks outside the web context, such as standalone graphical applications[11] and robotic drone control.[12] PHP code can also be directly executed from the command line.', 'image/blog/1734869936591909.png', '2022-06-05 22:43:54', '2022-06-06 02:05:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `blog_comments`
+--
+
+CREATE TABLE `blog_comments` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `user_id` bigint(20) UNSIGNED NOT NULL,
+  `blog_id` bigint(20) UNSIGNED NOT NULL,
+  `parent_id` bigint(20) UNSIGNED DEFAULT NULL,
+  `body` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -81,11 +123,13 @@ CREATE TABLE `migrations` (
 --
 
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
-(1, '2014_10_12_000000_create_users_table', 1),
-(2, '2014_10_12_100000_create_password_resets_table', 1),
-(3, '2019_08_19_000000_create_failed_jobs_table', 1),
-(4, '2019_12_14_000001_create_personal_access_tokens_table', 1),
-(7, '2022_06_02_041713_create_blogs_table', 2);
+(8, '2014_10_12_000000_create_users_table', 1),
+(9, '2014_10_12_100000_create_password_resets_table', 1),
+(10, '2019_08_19_000000_create_failed_jobs_table', 1),
+(11, '2019_12_14_000001_create_personal_access_tokens_table', 1),
+(12, '2022_06_02_061158_create_blogs_table', 1),
+(13, '2022_06_03_122029_create_blog_comments_table', 1),
+(14, '2022_06_04_083537_create_admins_table', 1);
 
 -- --------------------------------------------------------
 
@@ -125,10 +169,12 @@ CREATE TABLE `personal_access_tokens` (
 
 CREATE TABLE `users` (
   `id` bigint(20) UNSIGNED NOT NULL,
+  `role_as` int(11) NOT NULL DEFAULT 1,
   `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email_verified_at` timestamp NULL DEFAULT NULL,
   `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `picture` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
@@ -138,13 +184,21 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'Henil Gohil', 'henil.astech@gmail.com', NULL, '$2y$10$InmkMeFt1rr0FIC60M1EAePH6NZ9ppK3nZSWY61DaAjpRaJyCAmbG', NULL, '2022-06-01 23:54:19', '2022-06-01 23:54:19'),
-(2, 'Nikul', 'nikul@gmail.com', NULL, '$2y$10$GK7FZITGI1qTUcURVg.feeuOsbCsbW.A0H6nHV.NJ3jHXUsglvjuG', NULL, '2022-06-02 05:31:05', '2022-06-02 05:31:05');
+INSERT INTO `users` (`id`, `role_as`, `name`, `email`, `email_verified_at`, `password`, `picture`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 0, 'henil', 'henil@gmail.com', NULL, '$2y$10$M5.XhP5GMA1aVjJdZoVYnOM1VoBMaKhlBAwEgRmxIyRqdm1u2Rcp6', 'image/user/1734865354368477.jpeg', NULL, '2022-06-05 22:32:41', '2022-06-05 22:32:41'),
+(2, 0, 'Nikul', 'nikul@gmail.com', NULL, '$2y$10$jhl.K4a3VjPJjor4ob4dCOoYSbM3YXF7GGG4GHJGeyUOEREbdcUKK', NULL, NULL, '2022-06-05 22:40:01', '2022-06-05 22:40:01'),
+(3, 0, 'milan', 'milan@gmail.com', NULL, '$2y$10$vRcNpbkopQ85eiFqG63SReki9rvWp3NG6JXUenCZVPdOOF8wycVXG', NULL, NULL, '2022-06-05 22:42:52', '2022-06-05 22:42:52'),
+(4, 1, 'admin', 'admin@gmail.com', NULL, '$2y$10$M5.XhP5GMA1aVjJdZoVYnOM1VoBMaKhlBAwEgRmxIyRqdm1u2Rcp6', NULL, NULL, NULL, NULL);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `admins`
+--
+ALTER TABLE `admins`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `blogs`
@@ -152,6 +206,14 @@ INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `re
 ALTER TABLE `blogs`
   ADD PRIMARY KEY (`id`),
   ADD KEY `blogs_user_id_foreign` (`user_id`);
+
+--
+-- Indexes for table `blog_comments`
+--
+ALTER TABLE `blog_comments`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `blog_comments_user_id_foreign` (`user_id`),
+  ADD KEY `blog_comments_blog_id_foreign` (`blog_id`);
 
 --
 -- Indexes for table `failed_jobs`
@@ -192,10 +254,22 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `admins`
+--
+ALTER TABLE `admins`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `blogs`
 --
 ALTER TABLE `blogs`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `blog_comments`
+--
+ALTER TABLE `blog_comments`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `failed_jobs`
@@ -207,7 +281,7 @@ ALTER TABLE `failed_jobs`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `personal_access_tokens`
@@ -219,7 +293,7 @@ ALTER TABLE `personal_access_tokens`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Constraints for dumped tables
@@ -230,6 +304,13 @@ ALTER TABLE `users`
 --
 ALTER TABLE `blogs`
   ADD CONSTRAINT `blogs_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `blog_comments`
+--
+ALTER TABLE `blog_comments`
+  ADD CONSTRAINT `blog_comments_blog_id_foreign` FOREIGN KEY (`blog_id`) REFERENCES `blogs` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `blog_comments_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
